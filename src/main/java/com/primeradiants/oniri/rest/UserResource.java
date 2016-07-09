45770572922900a621fa0bb6b3f30018f720a538
Shanira <georges.biaux@prime-radiants.com>
2016-07-09 14:46:08 +0200

    Simplifying user endpoint

--- src/main/java/com/primeradiants/oniri/rest/UserResource.java
+++ src/main/java/com/primeradiants/oniri/rest/UserResource.java
@@ -1,17 +1,13 @@
 package com.primeradiants.oniri.rest;
 
-import java.util.ArrayList;
-import java.util.Collection;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.primeradiants.model.errors.ValidationError;
 import com.primeradiants.oniri.user.UserEntity;
 import com.primeradiants.oniri.user.UserManager;
 
@@ -28,41 +24,54 @@ import lombok.NoArgsConstructor;
 @RequestMapping("/rest/api")
 public class UserResource {
 
-	private static final String USERNAME = "username";
+//	private static final String USERNAME = "username";
 	
 	@Autowired private UserManager userManager;
 	
 	/**
-	 * Retrieve a user by its user name
-	 * @param username the user name of the user
-	 * @return a {@link com.primeradiants.oniri.rest.UserResource.UserResponse} if user exists, 
-	 * 			else a Collection of {@link com.primeradiants.model.errors.ValidationError}
+	 * Retrieves the current loged in user.
+	 * @return a {@link com.primeradiants.oniri.rest.UserResource.UserResponse}.
 	 */
-	@RequestMapping(value = "/user/{username}", method = RequestMethod.GET)
-	public ResponseEntity<?> getUser(@PathVariable(USERNAME) String username) 
+	@RequestMapping(value = "/user", method = RequestMethod.GET)
+	public ResponseEntity<?> getCurrentUser() 
 	{
-		final Collection<ValidationError> errors = new ArrayList<ValidationError>();
-		
-		//input validation
-		UserEntity user = validateUsername(username, errors);
-		
-		if (!errors.isEmpty())
-        {
-            return new ResponseEntity<Collection<ValidationError>>(errors, HttpStatus.BAD_REQUEST);
-        }
-		
+		UserDetails currentUser = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		UserEntity user = userManager.getUser(currentUser.getUsername());
 		return ResponseEntity.ok(new UserResponse(user.getUsername(), user.getEmail()));
 	}
 	
-	//Check if user name corresponds to an existing user in database and return the UserEntity object 
-	private UserEntity validateUsername(String username, Collection<ValidationError> errors) {
-		UserEntity user = userManager.getUser(username);
-		
-		if(user == null)
-			errors.add(new ValidationError(USERNAME, "Unknown user " + username));
-		
-		return user;
-	}
+//	/**
+//	 * Retrieve a user by its user name
+//	 * @param username the user name of the user
+//	 * @return a {@link com.primeradiants.oniri.rest.UserResource.UserResponse} if user exists, 
+//	 * 			else a Collection of {@link com.primeradiants.model.errors.ValidationError}
+//	 */
+//	@RequestMapping(value = "/user/{username}", method = RequestMethod.GET)
+//	public ResponseEntity<?> getUser(@PathVariable(USERNAME) String username) 
+//	{
+//		UserDetails currentUser = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//		final Collection<ValidationError> errors = new ArrayList<ValidationError>();
+//		
+//		//input validation
+//		UserEntity user = validateUsername(username, errors);
+//		
+//		if (!errors.isEmpty())
+//        {
+//            return new ResponseEntity<Collection<ValidationError>>(errors, HttpStatus.BAD_REQUEST);
+//        }
+//		
+//		return ResponseEntity.ok(new UserResponse(user.getUsername(), user.getEmail()));
+//	}
+	
+//	//Check if user name corresponds to an existing user in database and return the UserEntity object 
+//	private UserEntity validateUsername(String username, Collection<ValidationError> errors) {
+//		UserEntity user = userManager.getUser(username);
+//		
+//		if(user == null)
+//			errors.add(new ValidationError(USERNAME, "Unknown user " + username));
+//		
+//		return user;
+//	}
 	
 	@AllArgsConstructor
 	@NoArgsConstructor
