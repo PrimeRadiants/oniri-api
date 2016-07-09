8908919057ae8738c4a6a677f927d0086d23c425
Shanira <georges.biaux@prime-radiants.com>
2016-07-09 14:57:03 +0200

    Setting library/list endpoint

--- src/main/java/com/primeradiants/oniri/novent/NoventManager.java
+++ src/main/java/com/primeradiants/oniri/novent/NoventManager.java
@@ -1,5 +1,53 @@
 package com.primeradiants.oniri.novent;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.stereotype.Service;
+
+import com.primeradiants.hibernate.util.HibernateUtil;
+import com.primeradiants.oniri.user.UserEntity;
+
+/**
+ * Simple novent utility.
+ * @author Shanira
+ * @since 0.1.0
+ */
+@Service
 public class NoventManager {
 
+	private SessionFactory sessionFactory = HibernateUtil.getSessionAnnotationFactory();
+	private static final String USER = "user"; 
+	
+	/**
+	 * Return all the novents of the given user
+	 * @param user the user object
+	 * @return a List of  {@link com.primeradiants.oniri.novent.NoventEntity}
+	 */
+	@SuppressWarnings("unchecked")
+	public List<NoventEntity> getAllUserNovents(UserEntity user) {
+		List<NoventEntity> result = new ArrayList<NoventEntity>();
+		
+		if(user == null)
+			return result;
+		
+		Session session = sessionFactory.getCurrentSession();
+		session.beginTransaction();
+		
+		Criteria criteria = session
+			    .createCriteria(UserNoventEntity.class)
+			    .add(Restrictions.eq(USER, user));
+		
+		List<UserNoventEntity> userNoventEntities = (List<UserNoventEntity>) criteria.list();
+		
+		for(UserNoventEntity userNoventEntity : userNoventEntities)
+			result.add(userNoventEntity.getNovent());
+		
+		return result;
+	}
+	
 }
--- src/main/java/com/primeradiants/oniri/rest/LibraryResource.java
+++ src/main/java/com/primeradiants/oniri/rest/LibraryResource.java
@@ -1,9 +1,19 @@
 package com.primeradiants.oniri.rest;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.primeradiants.oniri.novent.NoventEntity;
+import com.primeradiants.oniri.novent.NoventManager;
+import com.primeradiants.oniri.user.UserEntity;
+import com.primeradiants.oniri.user.UserManager;
 
 /**
  * REST endpoints to manage a user novent library
@@ -14,8 +24,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/rest/api")
 public class LibraryResource {
 
+	@Autowired private UserManager userManager;
+	@Autowired private NoventManager noventManager;
+	
 	@RequestMapping(value = "/library/list", method = RequestMethod.GET)
 	public ResponseEntity<?> getLibraryNoventList() {
+		UserDetails currentUser = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		UserEntity user = userManager.getUser(currentUser.getUsername());
+		
+		List<NoventEntity> novents = noventManager.getAllUserNovents(user);
 		return null;
 	}
 	
